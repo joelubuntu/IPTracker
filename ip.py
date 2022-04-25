41eac5155b9c8309ff0631787b8fe8fc9dbe93c8
joelubuntu <72544077+joelubuntu@users.noreply.github.com>
2022-04-25 15:11:12 +0530

    Added things.
    Added - history feature in main menu and restart button in connection failed menu

--- ip.py
+++ ip.py
@@ -1,8 +1,30 @@
 from tkinter import *
 import urllib.request
 import json
+import os
+import sys
+from datetime import datetime
 
-def search_button_click():
+def restart():
+    file_name = sys.argv[0]
+    internet_error.destroy()
+    try:
+        try:
+            os.system('python '+ file_name)
+        except:
+            os.system('python3 '+ file_name)
+    except:
+        print('error occured while restarting')
+
+def make_history():
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    his_file = open('.history.txt','a')
+    his_file.write(dt_string + ' - ' + search_box.get() + '\n')
+    his_file.close()
+    search_results()
+
+def search_results():
 	root2 = Tk()
 	req_ip = urllib.request.urlopen('http://ip-api.com/json/' + search_box.get())
 	req_ip_json = json.loads(req_ip.read())
@@ -22,7 +44,11 @@ def search_button_click():
 	As = Label(root2,text=req_ip_json['as']).pack()
 	close_button = Button(root2, text="Close", command=root2.destroy).pack()
 	root2.mainloop()
-    
+
+def clear_history():
+    history = open('.history.txt','w')
+    history.write('')
+    history.close()
 
 try:
     user_ip = urllib.request.urlopen('https://api.ipify.org/?format=json')
@@ -31,14 +57,24 @@ try:
     user_ip_label = Label(main_menu,text= 'Your IP: ' + user_json_value['ip']).grid(row=0,column=0)
     search_box = Entry(main_menu,borderwidth=3)
     search_box.grid(row=1,column=0)
-    search_button = Button(main_menu,text='Search',command=search_button_click)
+    search_button = Button(main_menu,text='Search',command=make_history)
     search_button.grid(row=1,column=1)
-    exit_button = Button(main_menu, text="Exit", command=main_menu.destroy).grid(column=0, row=2)
+    exit_button = Button(main_menu, text="Exit", command=main_menu.destroy).grid(column=2, row=2)
     copy_button = Button(main_menu,text='Copy' ,command=main_menu.clipboard_append(user_json_value['ip'])).grid(row=0,column=1)
+    history_button = Button(main_menu,text='Clear History', command=clear_history).grid(column=1 , row=2)
+    history = Label(main_menu,text='History').grid(column=0,row=2)
+    try:
+        list = open('.history.txt','r')
+        Label(main_menu , text=list.read()).grid(column=0 , row=3)
+        list.close()
+    except:
+        create = open('.history.txt','a')
+        create.close()
     main_menu.mainloop()
 
 except:
     internet_error = Tk()
     error = Label(internet_error,text='Check Your Internet Connection!').pack()
     exit_button = Button(internet_error,text='Close',command=internet_error.destroy).pack()
+    restart_button = Button(internet_error,text='Restart',command=restart).pack()
     internet_error.mainloop()
